18917c0b87851bd23f43d2e8fcfcea466bc5c9ea
kadeball <kadeball@iu.edu>
2020-01-20 12:47:26 -0500

    1st commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<p>Name:Kalista</p>
+      <p>Ideal Job Title: Web Designer</p>
+      <p>Favorite Number: 3</p>
+    </div>
+  );
+}
+
+export default App;
